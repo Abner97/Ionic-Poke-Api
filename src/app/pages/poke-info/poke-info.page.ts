84d9a224fe94241b3c88f058625844bc1de6c5e9
Abraham <abrahamvega987@gmail.com>
2021-01-11 01:37:20 -0500

    Working on launchDarkly integration

--- src/app/pages/poke-info/poke-info.page.ts
+++ src/app/pages/poke-info/poke-info.page.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, NavigationStart, Router } from "@angular/router";
+import { AlertController, NavController } from "@ionic/angular";
 import { Pokemon } from "src/app/models/Pokemon";
 import { PokemonService } from "../../services/pokemon.service";
+import { LaunchdarklyService } from "src/app/services/launchdarkly.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-poke-info",
@@ -11,25 +14,85 @@ import { PokemonService } from "../../services/pokemon.service";
 export class PokeInfoPage implements OnInit {
   pokemon: Pokemon;
 
+  private pokemonInfoSubs: Subscription;
+  public pokemonInfoStatus: boolean;
+  private launcDarklyStatus: boolean;
+
   constructor(
     public router: Router,
     public route: ActivatedRoute,
-    public pokemonService: PokemonService
+    public navCtrl: NavController,
+    public pokemonService: PokemonService,
+    public launchDarkly: LaunchdarklyService,
+    public alertController: AlertController
   ) {
-    this.router.events.subscribe((e: NavigationStart) => {
-      try {
-        this.pokemon = this.router.getCurrentNavigation().extras
-          .state as Pokemon;
-      } catch (err) {
-        const pokemonName = this.route.snapshot.paramMap.get("pokemonName");
-        this.pokemonService.getPokemonWithName(pokemonName).then((pokemon) => {
-          this.pokemon = pokemon;
-        });
+    this.launchDarkly.checkServiceStatus().then((status) => {
+      if (status) {
+        this.pokemonInfoStatus = this.launchDarkly.checkForChanges(
+          "pokemon-info",
+          true
+        );
+        this.launcDarklyStatus = status;
+      } else {
+        this.pokemonInfoStatus = false;
       }
 
-      console.log(this.pokemon);
+      if (!this.pokemonInfoStatus) {
+        this.showAlert();
+      } else {
+        console.log("entre");
+        this.router.events.subscribe((e: NavigationStart) => {
+          try {
+            this.pokemon = this.router.getCurrentNavigation().extras
+              .state as Pokemon;
+          } catch (err) {
+            const pokemonName = this.route.snapshot.paramMap.get("pokemonName");
+            this.pokemonService
+              .getPokemonWithName(pokemonName)
+              .then((pokemon) => {
+                this.pokemon = pokemon;
+              });
+          }
+
+          console.log(this.pokemon);
+        });
+      }
     });
   }
 
+  showAlert() {
+    this.alertController
+      .create({
+        header: "Anuncio",
+        subHeader: "",
+        message:
+          "Este servicio a sido inhabilitado temporalmente, lamentamos las molestias",
+        buttons: [
+          {
+            text: "Ok",
+            role: "ok",
+            handler: (data) => {
+              this.navCtrl.navigateRoot(["/home"], {
+                state: {},
+                animated: true,
+              });
+            },
+          },
+        ],
+      })
+      .then((res) => {
+        res.present();
+      });
+  }
+
+  ionViewDidEnter(): void {
+    if (this.launcDarklyStatus) {
+      this.pokemonInfoSubs = this.launchDarkly
+        .getBooleanFlagObservable("pokemon-info")
+        .subscribe((enabled) => {
+          this.pokemonInfoStatus = enabled;
+        });
+    }
+  }
   ngOnInit() {}
 }
--- src/app/services/launchdarkly.service.ts
+++ src/app/services/launchdarkly.service.ts
@@ -5,41 +5,59 @@ import {
   LDClientBase,
   LDUser,
 } from "launchdarkly-js-client-sdk";
-import { Observable } from "rxjs";
+import { Observable, Subject } from "rxjs";
+
 @Injectable({
   providedIn: "root",
 })
 export class LaunchdarklyService {
   private ldClient: LDClientBase;
+  public serviceReady: boolean;
   private user: LDUser;
 
   constructor() {
     this.initialize();
-    this.ldClient.on("ready", () => {
-      console.log("It's now safe to request feature flags");
-      var showFeature = this.ldClient.variation("pokemon-info", true);
-      if (showFeature) {
-        console.log("is on");
-      } else {
-        console.log("is of");
-      }
-    });
+  }
+
+  getBooleanFlagObservable(flagName: string) {
+    let booleanFlag: Subject<boolean> = new Subject<boolean>();
+    let booleanFlagObs: Observable<boolean> = booleanFlag.asObservable();
+    this.listenChangesOnServer(flagName, true, booleanFlag);
+    return booleanFlagObs;
+  }
+
+  async checkServiceStatus(): Promise<boolean> {
+    try {
+      await this.ldClient.waitForInitialization();
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  public checkForChanges(flagName: string, defaultValue: any) {
+    this.ldClient.on("ready", () => {});
+    return this.ldClient.variation(flagName, defaultValue);
+  }
+
+  listenChangesOnServer(
+    flagName: string,
+    defaultValue: any,
+    subject: Subject<any>
+  ) {
     this.ldClient.on("change", () => {
-      var showFeature = this.ldClient.variation("pokemon-info", true);
-      if (showFeature) {
-        console.log("is on");
-      } else {
-        console.log("is of");
-      }
+      var featureStatus = this.checkForChanges(flagName, defaultValue);
+      subject.next(featureStatus);
     });
   }
 
   initialize() {
     this.user = {
       key: "aa0ceb",
+
+      email: "abrahamvega987@gmail.com",
+      country: "Panamá",
     };
     this.ldClient = LDClient.initialize("5ff5da42a793ec0abb9f7267", this.user);
   }
-
-  ngOnInit(): void {}
 }
